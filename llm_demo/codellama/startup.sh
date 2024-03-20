8a04514a598e6cf61bf638b473f8d99bd373852d
Ari Chadda <59665721+arichadda@users.noreply.github.com>
2024-03-20 15:16:26 -0400

    victim model build process stable

--- llm_demo/codellama/startup.sh
+++ llm_demo/codellama/startup.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 screen -dm ollama serve
 sleep 1
-ollama pull $VICTIM_MODEL_NAME
-echo "model pulled " $VICTIM_MODEL_NAME
-echo "file path " $VICTIM_MODEL_FILE_PATH
-ollama create $VICTIM_SERVICE_NAME -f $VICTIM_MODEL_FILE_PATH
-echo "revised model created " $VICTIM_SERVICE_NAME
-ollama run $VICTIM_SERVICE_NAME
+ollama pull $MODEL_NAME
+echo "model pulled" $MODEL_NAME
+echo "file path" $MODEL_FILE_NAME
+ollama create $SERVICE_NAME -f $MODEL_FILE_NAME
+echo "revised model created" $SERVICE_NAME
+ollama run $SERVICE_NAME
